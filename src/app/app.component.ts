860ce1999a0d40dd2466d379810491702924c703
Lance Finney <lance.finney@oasisdigital.com>
2023-12-15 16:36:33 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ export class AppComponent {
   // Implicit public, equivalent to
   // public employee: string;
   // (public is the default in TypeScript.)
-  // Angular.io style guide recommends implicit public.
 
   // If the value is known at initialization, it is ok to do so here.
   // Later we will see the constructor used to set these properties
